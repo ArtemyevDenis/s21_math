c554afe94787ddb79340212d26d44bd241f42c22
slughorl <slughorl@student.21-school.ru>
2023-12-14 05:17:35 +0500

    develop added tests

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <check.h>
 
-#define PI 3.14159265359
+#define PI 3.141592653589793238462643383279 
 
-long long int phactorial(int n){
-    long long int rez = 1;
+double phactorial(int n){
+    double rez = 1.0;
     if(n == 0 || n == 1)
         rez = 1;
     else
@@ -22,10 +23,10 @@ long double s21_cos(double x){
     if(x < 0)
         x = -x;
     while(x >= 2*PI)
-        x -= 2*PI;
+        x -= (double)2*PI;
     if (x >= PI)
-        x = PI - (x-PI);
-    long double res = 0.0;
+        x = (double)(PI - (x-PI));
+    double res = 0.0;
     for(int i = 0; i < 11; i++){
         res += pow(-1.0,i) * pow(x,2.0*i)/phactorial(2*i);
     }
@@ -43,27 +44,57 @@ long double s21_sin(double x){
     if (x >= PI/2)
         x = PI/2 - (x-PI/2);
     long double res = 0.0;
-    for(int i = 0; i < 11; i++){
-        res += pow(-1.0,i) * pow(x,2.0*i + 1)/phactorial(2*i + 1);
+    for(int i = 0; i < 250000; i++){
+        res += pow(-1.0,i) * pow(x,2.0*i + 1)/(long double)phactorial(2*i + 1);
     }
     return minus*res;
 }
 
-int main(){
-    int count_suc = 0, count_fail = 0;
-    for(double i = -100.0; i < 100.0; i+=0.10){
-        
-        long double temp = 0;
-        double temp_m = 0;
+// Функция тестирования какой-либо задачи.
+START_TEST(test_name)
+{
+    for(double i = 0.0; i < 1.0; i+=0.10){
+        long double temp = 0.0;
+        double temp_m = 0.0;
         temp = s21_cos(i);
         temp_m = cos(i);
-        if(fabs(temp - temp_m) <= 0.0000001)
-            // printf("SUCCESS ||  s21: %Lf ||  math: %lf || i: %lf\n",temp,temp_m,i);
-            count_suc++;
-        else{
-            printf("FAIL ||  s21: %Lf ||  math: %lf || i: %lf\n",temp,temp_m,i);
-            count_fail++;
-        }
+        //printf("s21 == %.20LF |||  math == %.20lf\n",temp, temp_m);
+        ck_assert_ldouble_eq_tol(temp,temp_m,0.000000000000001);
     }
-    printf("SUCCESS: %d\nFAIL: %d\n", count_suc, count_fail);
+}
+END_TEST
+
+// Функция создания набора тестов.
+Suite *example_suite_create(void)
+{
+    Suite *suite = suite_create("Example");
+    // Набор разбивается на группы тестов, разделённых по каким-либо критериям.
+    TCase *tcase_core = tcase_create("Core of example");
+    
+    // Добавление теста в группу тестов.
+    tcase_add_test(tcase_core, test_name);
+    
+    // Добавление теста в тестовый набор.
+    suite_add_tcase(suite, tcase_core);
+    
+    return suite;
+}
+
+int main(void)
+{
+    Suite *suite = example_suite_create();
+    SRunner *suite_runner = srunner_create(suite);
+    int failed_count = 0;
+    
+    srunner_run_all(suite_runner, CK_NORMAL);
+    // Получаем количество проваленных тестов.
+    failed_count = srunner_ntests_failed(suite_runner);
+    srunner_free(suite_runner);
+    
+    if (failed_count != 0) {
+        // Сигнализируем о том, что тестирование прошло неудачно.
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
